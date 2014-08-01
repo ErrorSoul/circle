dc54bbd41da7debd9fd4e79c0e307928f9795b23
Abu Sultanov <neurofag@gmail.com>
2014-08-01 08:15:35 +0400

    f

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,8 +29,7 @@ Circle::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  config.assets.precompile = ['admin.js', 'admin.css']
-
+  
   # Generate digests for assets URLs.
   config.assets.digest = true
 
